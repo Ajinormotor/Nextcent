1ee77a07c2d1b594bab499f798e37f84c7841db4
Teitei Wisdom <teiteiwisdom@gmail.com>
2024-01-28 01:26:57 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+const navlink  = document.getElementById("navlink");
+
+
+
+
+function showMenu() {
+    navlink.style.left = "0px"
+    console.log("Yes it has been clicked");
+}
+
+function hideMenu() {
+    navlink.style.left="-400px"
+    console.log("Yes it has been clicked")
+}
