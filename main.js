e19a7e764bc45945f16b26e0e8f1ad7265b00c5f
Teitei Wisdom <teiteiwisdom@gmail.com>
2024-04-04 08:47:03 +0100

    Added footer and 0ne more section

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
 const navlink  = document.getElementById("navlink");
 
 
-
-
 function showMenu() {
     navlink.style.left = "0px"
     console.log("Yes it has been clicked");
 }
 
 function hideMenu() {
-    navlink.style.left="-400px"
+    navlink.style.left="-500px"
     console.log("Yes it has been clicked")
 }
+
+
+let header = document.querySelector('header');
+
+window.addEventListener('scroll',()=>{
+    header.classList.toggle('shadow',window.scrollY > 0);
+    console.log("Window Event Listener is active");
+})
